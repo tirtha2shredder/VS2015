21bf935438350ee65011620a68a62dee4841d7ab
uttariya <uttariya@outlook.com>
2015-11-30 02:16:04 +0530

    grades visible on clicking grades

--- LoginUniversalApp1/graded.xaml.cs
+++ LoginUniversalApp1/graded.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,30 @@ namespace LoginUniversalApp1
     /// </summary>
     public sealed partial class graded : Page
     {
+        public String x;
         public graded()
         {
             this.InitializeComponent();
+            Loaded += new RoutedEventHandler(page_loaded);
+
         }
+        async void page_loaded(object sender, RoutedEventArgs e)
+        {
+            List<PendJson> transaction = new List<PendJson>();
+            await Task.Run(() => {
+                //x = File.ReadAllText(@"C:\Users\uttariya bandhu\Source\Repos\VS2015\LoginUniversalApp1\Assets\sublist.json");
+                x = "[{\"name\":\"phy\",\"loc\":\"50\",\"\":null},{\"name\":\"chem\",\"loc\":\"60\",\"\":null},{\"name\":\"math\",\"loc\":\"70\",\"\":null}]";
+                JArray obj = JArray.Parse(x);
+                for (int i = 0; i < obj.Count; i++)
+                {
+
+                    JObject row = JObject.Parse(obj[i].ToString());
+                    transaction.Add(new PendJson(row["name"].ToString(), row["loc"].ToString()));
+
+                }
+            });
+            listBox.ItemsSource = transaction;
+        }
+
     }
 }
