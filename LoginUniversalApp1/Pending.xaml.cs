4cda65892aab600e73a8a157730b7ec5be4f0939
uttariya <uttariya@outlook.com>
2015-11-30 01:41:48 +0530

    listbox population dynamically from json data done.the only problem that is occuring is that the jason file can't be opened and i get a unauthorizedAccess exception not only in c drive but also in all other drives.

--- LoginUniversalApp1/Pending.xaml.cs
+++ LoginUniversalApp1/Pending.xaml.cs
@@ -12,6 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,18 +26,28 @@ namespace LoginUniversalApp1
     /// </summary>
     public sealed partial class Pending : Page
     {
+        public String x;
         public Pending()
         {
-           
             this.InitializeComponent();
             Loaded += new RoutedEventHandler(page_loaded);
            
         }
-        void page_loaded(object sender,RoutedEventArgs e)
+        async void page_loaded(object sender,RoutedEventArgs e)
         {
             List<PendJson> transaction = new List<PendJson>();
-            
-    
+            await Task.Run(() => {
+                //x = File.ReadAllText(@"C:\Users\uttariya bandhu\Source\Repos\VS2015\LoginUniversalApp1\Assets\sublist.json");
+                x = "[{\"name\":\"phy\",\"loc\":\"#\",\"\":null},{\"name\":\"chem\",\"loc\":\"#\",\"\":null},{\"name\":\"math\",\"loc\":\"#\",\"\":null}]";
+                JArray obj = JArray.Parse(x);
+            for (int i = 0; i < obj.Count; i++)
+            {
+
+                JObject row = JObject.Parse(obj[i].ToString());
+                transaction.Add(new PendJson(row["name"].ToString(), row["loc"].ToString()));
+
+                }
+            });
             listBox.ItemsSource = transaction;
         }
      
